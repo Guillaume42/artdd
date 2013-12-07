17089be0f4eae05a0d54deb454da2daf0d2864bf
André Aubin <andre.aubin@lambdaweb.fr>
2013-12-07 02:15:56 +0100

    Premier jet, bien moche

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="L'art d'aider">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="ico/favicon.png">
+
+    <title>L'art d'aider</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron-narrow.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="navbar navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+          <li><a href="#">Déposer un annonce</a></li>
+          <li><a href="#">Offres</a></li>
+          <li><a href="#">Association</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+<div class="container">
+<!--
+      <div class="jumbotron">
+        <h1>Jumbotron heading</h1>
+        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+      </div>
+-->
+      <div class="row marketing">
+
+        <!-- Première colonne -->
+        <article class="gallery col-lg-12">
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/001.jpg">
+        </div>
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/003.jpg">
+        </div>
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/002.jpg">
+        </div>
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/003.jpg">
+        </div>
+      </article>
+
+        <!-- Deuxieme colonne -->
+        <article class="gallery col-lg-12">
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/004.jpg">
+        </div>
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/005.jpg">
+        </div>
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/006.jpg">
+        </div>
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/007.jpg">
+        </div>
+      </article>
+
+        <!-- Barre de recherche -->
+        <div class="col-lg-12">
+          <div class="col-lg-6 centered">
+            <div>
+              <form id="form-search">
+                <input type="test" id="search-bar" name="search-bar">
+                <button id="search-button" name="search-button"><span class="glyphicon glyphicon-search"></span></button>
+              </form>
+            </div>
+            <div>0€ Reversés</div>
+          </div>
+        </div>
+
+        <!-- Troisieme colonne -->
+        <article class="gallery col-lg-12">
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/008.jpg">
+        </div>
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/009.jpg">
+        </div>
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/010.jpg">
+        </div>
+        <div class="col-lg-3">
+            <img class="img-rounded" src="img/thumbs/011.jpg">
+        </div>
+      </article>
+
+      </div>
+
+      <div class="footer">
+        <p>&copy; Guillaume Sarfati, Conrad Kleinespel & André Aubin 2013</p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
